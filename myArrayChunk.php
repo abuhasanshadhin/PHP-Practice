662e5ff34cc5cacdac323ef3e3888c5fc0b7b8a4
Abu Hasan Shadhin <49744087+abuhasanshadhin@users.noreply.github.com>
2020-10-27 16:15:51 +0600

    Update myArrayChunk.php

--- myArrayChunk.php
+++ myArrayChunk.php
@@ -1,3 +1,7 @@
+<?php
+
+$input_array = array('a', 'b', 'c', 'd', 'e', 'f', 'g');
+
 function myArrayChunk($array, $size, $preserve_keys = false) {
     $i = $j = 0;
     $newArray = [];
@@ -16,3 +20,38 @@ function myArrayChunk($array, $size, $preserve_keys = false) {
     }
     return $newArray;
 }
+
+print_r(myArrayChunk($input_array, 2));
+
+?>
+
+
+
+
+// Output
+
+Array
+(
+    [0] => Array
+        (
+            [0] => a
+            [1] => b
+        )
+
+    [1] => Array
+        (
+            [0] => c
+            [1] => d
+        )
+
+    [2] => Array
+        (
+            [0] => e
+            [1] => f
+        )
+
+    [3] => Array
+        (
+            [0] => g
+        )
+)
